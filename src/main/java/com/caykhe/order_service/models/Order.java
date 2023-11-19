6a13c09b402c86bc0a7370da58188e9970176bad
ThanhQuoc0719 <thanhquocnguyen1907@gmail.com>
2023-11-19 21:03:43 +0700

    add .

--- src/main/java/com/caykhe/order_service/models/Order.java
+++ src/main/java/com/caykhe/order_service/models/Order.java
@@ -1,4 +1,19 @@
 package com.caykhe.order_service.models;
 
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Builder
+@Document(collection = "order")
 public class Order {
+    private String id;
+    private String userId;
+    private Date orderDate;
+    private Double totalAmount;
+
 }
--- src/main/java/com/caykhe/order_service/models/OrderHistory.java
+++ src/main/java/com/caykhe/order_service/models/OrderHistory.java
@@ -1,4 +1,17 @@
 package com.caykhe.order_service.models;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Builder
+@Document(collection = "orderHistory")
 public class OrderHistory {
+    private String id;
+    private String orderId;
+    private String status;
+    private Date updateAt;
 }
